d0e6de22655ce36f3d0757ecc4445676431c582a
hahas507 <aq12361@gmail.com>
2021-07-24 00:51:16 +0900

    Challenge 2.1
    Dogs are spawning at the top of the screen when it's supposed to spawn balls. - Fixed
    Also fixed player spawning balls instead of dogs(which was going to be Challenge 2.2).

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
