4ea921a1165ac327108bf0fec764323aa627568c
hahas507 <aq12361@gmail.com>
2021-07-23 19:50:48 +0900

    2.1.6 Clean up your code and variables

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+
+    [SerializeField]
+    private float speed = 10f;
+
+    [SerializeField]
+    private float xRange = 10f;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
