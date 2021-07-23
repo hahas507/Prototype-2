770fc577db29abb5c134d23d47138206bb787954
hahas507 <aq12361@gmail.com>
2021-07-23 21:39:36 +0900

    2.3.2 Spawn an animal if S is pressed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,18 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject[] animalPrefabs;
 
+    [SerializeField]
+    private int animalIndex;
+
     private void Start()
     {
     }
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
