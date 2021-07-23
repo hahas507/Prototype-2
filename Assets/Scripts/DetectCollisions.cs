2eadbe2aeaac200c66d7631c8cd490edc2625625
hahas507 <aq12361@gmail.com>
2021-07-24 00:29:00 +0900

    2.4.4 Destroy objects on collision

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
