5d7d92f743edd0776ecf5d7ecfb1116b9e938490
hahas507 <aq12361@gmail.com>
2021-07-23 20:19:00 +0900

    2.2.6 Destroy projectiles offscreen

---
+++ Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    [SerializeField]
+    private float topBound = 30f;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
