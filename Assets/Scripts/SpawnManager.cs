cd358fd2df3e95fff9ac58a64272437531e6a9d0
hahas507 <aq12361@gmail.com>
2021-07-23 21:33:33 +0900

    2.3.1 Create a spawn manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] animalPrefabs;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
