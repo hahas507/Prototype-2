976a377d33a93205e0985c781d500e7542141f6d
hahas507 <aq12361@gmail.com>
2021-07-23 20:06:23 +0900

    2.2.3 Test for spacebar press

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,7 +29,13 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //플레이어가 투사체를 발사
+        }
     }
 }
